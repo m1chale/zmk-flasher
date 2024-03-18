6ce1ce1c2e3ea048f3a34bc5c519b7e9e91b6c95
Neuer <dominik.schraml97@gmail.com>
2024-03-19 00:27:19 +0100

    improved verbose output

--- ZmkFlasher/ZmkFlasher/WaitRemovableDevice/Wait.cs
+++ ZmkFlasher/ZmkFlasher/WaitRemovableDevice/Wait.cs
@@ -10,11 +10,10 @@ internal static class Wait
         {
             foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
-                if(verbose) Console.WriteLine($"Drive {drive.Name} {drive.DriveType} {drive.VolumeLabel}");
-                if (drive.DriveType == DriveType.Removable && drive.VolumeLabel == volumeLabel)
-                {
-                    return new Directory.Info(drive.RootDirectory.FullName);
-                }
+                if(drive.DriveType != DriveType.Removable) continue;
+                if (verbose) Console.WriteLine($"Drive-Label: {drive.VolumeLabel}");
+                if (drive.VolumeLabel != volumeLabel) continue;
+                return new Directory.Info(drive.RootDirectory.FullName);
             }
             await Task.Delay(1000);
         }
