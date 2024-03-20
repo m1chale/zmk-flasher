767f4f36c9f6e74e3431af7963657363d1196d81
Neuer <dominik.schraml97@gmail.com>
2024-03-20 01:19:54 +0100

    remove unused code

--- ZmkFlasher/ZmkFlasher/Program.cs
+++ ZmkFlasher/ZmkFlasher/Program.cs
@@ -4,12 +4,6 @@ using ZmkFlasher.Arguments;
 using ZmkFlasher.Lib;
 using ZmkFlasher.WaitRemovableDevice;
 
-var drives = await Lsblk.Run();
-foreach (var drive in drives)
-{
-    Console.WriteLine(drive);
-}
-
 var result = CommandLine.Parser.Default.ParseArguments<StringArguments>(args);
 if (result.Errors.Any()) throw new Exception($"invalid arguments {result}");
 var arguments = result.Value.ToTypedArguments();
