c61cb87fe8eecf6f552383708920b1d4915d2a97
m1chale <mfriedrich3@outlook.de>
2025-04-06 22:20:17 +0200

    add darwin support

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"os"
+	"runtime"
 	"zmk-flasher/cmd"
 	"zmk-flasher/platform"
 )
 
 func main() {
 
-	platform.Os = platform.LinuxOsOperations{}
+	switch runtime.GOOS {
+	case "darwin":
+		platform.Os = platform.DarwinOperations{}
+	case "linux":
+		platform.Os = platform.LinuxOsOperations{}
+	default:
+		println("OS not supported yet")
+		os.Exit(1)
+	}
+
 	err := cmd.Execute()
 	if err != nil {
 		println(err.Error())
 		os.Exit(1)
 	}
 }
-
